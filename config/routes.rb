105abc381e6d202d49867043ca1a49f8cae86a4a
jiayen lee <jiiayenn.lee@gmail.com>
2021-03-03 22:26:16 +0800

    created _new.html.erb and updated style

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :restaurants
-  # get "restaurants", to: "restaurants#index"
-  # get "restaurants/new", to: "restaurants#new"
-  # post "restaurants", to: "restaurants#create"
-  # get "restaurants/:id", to: "restaurants#show"
-
-
+  
 
 end
