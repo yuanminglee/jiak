96daa24adbbbbc3661b5e21c14006047d5781877
jasonleewkd <jasonleewkd@gmail.com>
2021-03-06 12:25:37 +0800

    created feature to create restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,21 @@
 class RestaurantsController < ApplicationController
   before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   def edit
